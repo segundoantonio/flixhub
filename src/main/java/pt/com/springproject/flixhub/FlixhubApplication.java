ad2beea74661c7d27b2e4627ee8ac41cdb1f1499
segundoantonio <antoniohsegundo@gmail.com>
2023-10-16 20:06:03 +0100

    First commit

---
+++ src/main/java/pt/com/springproject/flixhub/FlixhubApplication.java
@@ -0,0 +1,13 @@
+package pt.com.springproject.flixhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixhubApplication.class, args);
+	}
+
+}
