2f1665b147ad0d82111ccc1490127c8cf0edecf4
segundoantonio <antoniohsegundo@gmail.com>
2023-10-19 19:47:21 +0100

    fix error receiving data

--- src/main/java/pt/com/springproject/flixhub/controller/MovieController.java
+++ src/main/java/pt/com/springproject/flixhub/controller/MovieController.java
@@ -2,7 +2,9 @@ package pt.com.springproject.flixhub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pt.com.springproject.flixhub.domain.movies.registrationDataMovies;
 
 @Controller
 @RequestMapping("/movies")
@@ -10,6 +12,12 @@ public class MovieController {
 
     @GetMapping
     public String loadFormPage() {
+        return "movies/form";
+    }
+
+    @PostMapping
+    public String registerMovies(registrationDataMovies data) {
+        System.out.println(data);
         return "movies/form";
     }
 }
---
+++ src/main/java/pt/com/springproject/flixhub/domain/movies/registrationDataMovies.java
@@ -0,0 +1,4 @@
+package pt.com.springproject.flixhub.domain.movies;
+
+public record registrationDataMovies(String name, Integer duration, Integer year, String genre) {
+}
